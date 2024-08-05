571405fbe6fd5bf0f7aa363d5b332d38d76355c9
alysson <alyssonmarimo@gmail.com>
2024-08-05 00:43:33 -0300

    atualizaçao de transition

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function toggleMode(){
-    const html = document.body;
-    const img = document.querySelector(".Profile-Home img")
-  
-      html.classList.toggle("light")
-     if(html.classList.contains('light')){
-      img.setAttribute("src", "./assets/octocat-1721685940495.png")
-    }else{
-      img.setAttribute("src", "./assets/WhatsApp Image 2024-07-14 at 12.39.29.jpeg")
-     
-    } 
-   
+  function toggleMode(){
+  const body = document.body;
+  const img = document.querySelector(".Profile-Home img")
+  const h1 = document.querySelector(".description h1");
+    
+  body.classList.toggle("light");
+      if(body.classList.contains('light')){
+        h1.innerHTML = 'Me Segue Aí <span><i class="fa-regular fa-circle-check"></i></span>'
+        img.setAttribute("src", "./assets/octocat-1721685940495.png")
+  }else{
+        h1.innerHTML = 'Alisson Sousa <span><i class="fas fa-check-circle"></i></span>';
+        img.setAttribute("src", "./assets/WhatsApp Image 2024-07-14 at 12.39.29.jpeg")
+  }
   }
